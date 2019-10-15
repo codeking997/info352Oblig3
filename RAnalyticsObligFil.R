5bf18fc050297e9e532e044c5703eada72993d0f
kingofthenorth871 <35599641+kingofthenorth871@users.noreply.github.com>
2019-10-15 12:04:34 +0200

    ny

---
+++ RAnalyticsObligFil.R
@@ -0,0 +1,19 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: ASUS g751jt
+# Created on: 15.10.2019
+
+# ----- SCATTERPLOTS -----
+# Use a scatterplot to see if batting average is directly
+# connected to runs produced
+# Load player data
+# if, else and else if works like other languages
+age = 18
+
+if(age >= 18) {
+  print("Drive and Vote")
+} else if (age >= 16){
+  print("Drive")
+} else {
+  print("Wait")
+}
